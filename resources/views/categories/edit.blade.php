fc271ff8667036c6d4138512d9fee9fbd7b618f5
Guilherme Cunha <guilherme@alugoaki.com.br>
2017-02-17 00:39:06 -0200

    first commit

---
+++ resources/views/categories/edit.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h3>Editar Categoria</h3>
+
+            {!! Form::model($category, [
+                    'route' => ['categories.update', 'category' => $category->id
+                    ],'class' => 'form', 'method' => 'PUT']) !!}
+
+            @include('categories._form')
+
+            {!! Html::openFormGroup() !!}
+                {!! Button::primary('Atualizar categoria')->submit() !!}
+            {!! Html::closeFormGroup() !!}
+
+            {!! Form::close() !!}
+        </div>
+    </div>
+@endsection
